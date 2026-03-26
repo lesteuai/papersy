4797607fdc51d608f6a1eb63096b4c93824905b7
bachsofttrick <xuanbach1307@gmail.com>
2026-03-26 07:49:13 -0700

    Change importing with fs as async form, add loop run for daignosing the pipeline

--- index.ts
+++ index.ts
@@ -1,30 +1,47 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import { PDFParse } from 'pdf-parse';
 import OpenAI from 'openai';
 
+const OPENAI_URL = 'http://localhost:8033/v1';
+const PDF_URL = './report.pdf';
+const PROMPT_URL = './summarize_prompt.txt';
+
 async function main() {
-  const parser = new PDFParse({ url: './report.pdf' });
+  // Import the PDF and system prompt
+  const parser = new PDFParse({ url: PDF_URL });
   const { text: paperText } = await parser.getText();
-
-  const promptTemplate = fs.readFileSync('./summarize_prompt.txt', 'utf-8');
-  const prompt = promptTemplate.replace('[INSERT PAPER TEXT HERE]', paperText);
-
+  const prompt = await fs.readFile(PROMPT_URL, {encoding: 'utf-8'});
   const client = new OpenAI({
     apiKey: 'dummy',
-    baseURL: 'http://localhost:8033/v1',
+    baseURL: OPENAI_URL,
   });
 
   const completion = await client.chat.completions.create({
     model: 'qwen3',
-    messages: [{ role: 'user', content: prompt }],
+    messages: [
+      { role: 'system', content: prompt },
+      { role: 'user', content: paperText }
+    ],
   });
-
   let result = completion.choices[0].message.content as string;
 
   // Strip JSON backticks before parsing
   result = result.replace(/```json|```/g, "").trim();
-
-  console.log(result as string);
+  
+  console.log(JSON.parse(result));
 }
 
-main();
+let count = 0;
+async function loop() {
+  while (true) {
+    count += 1;
+    console.log(`\n[${count}]:`);
+      try {
+      await main();
+    } catch (e) {
+      console.log('[parse error]:', e);
+    }
+  }
+}
+
+loop()
